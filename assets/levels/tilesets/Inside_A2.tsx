400362233200e427315adec90c11377994cdc3d3
skipdudes <skipdudes.dev@gmail.com>
2026-07-05 05:02:39 +0200

    Add mage house level

--- assets/levels/tilesets/Inside_A2.tsx
+++ assets/levels/tilesets/Inside_A2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="Inside_A2" tilewidth="48" tileheight="48" tilecount="208" columns="16">
- <image source="../../images/tilesets/Inside_A2.png" width="768" height="624"/>
+<tileset version="1.10" tiledversion="1.12.2" name="Inside_A2" tilewidth="48" tileheight="48" tilecount="240" columns="16">
+ <image source="../../images/tilesets/Inside_A2.png" width="768" height="720"/>
 </tileset>
