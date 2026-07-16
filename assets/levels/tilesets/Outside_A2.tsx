be7614bd7d838bae013891cb3dae7b555faaece9
skipdudes <skipdudes.dev@gmail.com>
2026-07-16 17:05:18 +0200

    Add remaining levels

--- assets/levels/tilesets/Outside_A2.tsx
+++ assets/levels/tilesets/Outside_A2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="Outside_A2" tilewidth="48" tileheight="48" tilecount="192" columns="16">
- <image source="../../images/tilesets/Outside_A2.png" width="768" height="576"/>
+<tileset version="1.10" tiledversion="1.12.2" name="Outside_A2" tilewidth="48" tileheight="48" tilecount="224" columns="16">
+ <image source="../../images/tilesets/Outside_A2.png" width="768" height="672"/>
 </tileset>
--- assets/levels/tilesets/Outside_A4.tsx
+++ assets/levels/tilesets/Outside_A4.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="Outside_A4" tilewidth="48" tileheight="48" tilecount="240" columns="16">
- <image source="../../images/tilesets/Outside_A4.png" width="768" height="720"/>
+<tileset version="1.10" tiledversion="1.12.2" name="Outside_A4" tilewidth="48" tileheight="48" tilecount="272" columns="16">
+ <image source="../../images/tilesets/Outside_A4.png" width="768" height="816"/>
 </tileset>
